4cb52498fdc5388aeaa18a20a594ce850ddc5d15
wenqian157 <44199787+wenqian157@users.noreply.github.com>
2022-03-25 09:02:34 +0100

    update

---
+++ subd_city4_rule_b.py
@@ -0,0 +1,41 @@
+import sys
+import os
+
+SCRIPT_DIR = os.path.dirname(__file__)
+sys.path.append(os.path.dirname(SCRIPT_DIR))
+import mola 
+
+mesh_city = mola.Mesh()
+
+a = mesh_city.add_vertex(0, 0, 0)
+b = mesh_city.add_vertex(100, 0, 0)
+c = mesh_city.add_vertex(100, 100, 0)
+d = mesh_city.add_vertex(0, 100, 0)
+mesh_city.add_face([a, b, c, d])
+
+engine = mola.Engine(mesh_city)
+
+engine.rules.append([{"select":"block", "ratio": 1.0}, {"subd": "face_split_grid", "arg": [2, 3]}])
+engine.rules.append([{"select":"block_s", "ratio": 1.0}, {"subd": "mesh_catmull", "arg": []}])
+engine.rules.append([{"select":"plot", "ratio": 0.8}, {"subd": "face_extrude_tapered", "arg": [0, 0.3]}])
+engine.rules.append([{"select":"construct_up", "ratio": 0.85}, {"subd": "face_extrude_tapered", "arg": [5, 0]}])
+engine.rules.append([{"select":"construct_side", "ratio": 0.2}, {"subd": "face_extrude_tapered", "arg": [3, 0]}])
+engine.rules.append([{"select":"wall", "ratio": 0.8}, {"subd": "face_split_cell", "arg": [0.8, 3]}])
+engine.rules.append([{"select":"panel", "ratio": 0.7}, {"subd": "face_extrude_tapered", "arg": [0, 0.3]}])
+
+# n = 15
+# for i in range(n):
+#     if i <=3:
+#         engine.subdivide_block()
+#     elif i >= n - 2:
+#         engine.subdivide_facade()
+#     else:
+#         engine.subdivide_building()
+
+engine.subdivide_block(2)
+engine.subdivide_building(12)
+engine.subdivide_facade(2)
+
+mesh_city = engine.mesh
+print(len(mesh_city.faces))
+mola.export_obj(mesh_city, "my_city2.obj")
--- test.py
+++ test.py
@@ -1,4 +1,1 @@
-dic = {"a": 1, "b": 2}
-
-lis = [5, 8 ,10]
-print(lis.index(5))
+print(1)
